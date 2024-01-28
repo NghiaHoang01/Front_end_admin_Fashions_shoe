90abc29d1b41cca14ddaf91fe45c77459877e196
NghiaHoang01 <bebebechecheche@gmail.com>
2024-01-28 15:16:28 +0700

    update url api updateInformation

--- src/service/AccountService.jsx
+++ src/service/AccountService.jsx
@@ -1,7 +1,7 @@
 import request from "utils/Request"
 
 export const updateInformationService = (params) => {
-    return request('/api/user/update/profile', {
+    return request('/api/admin/update/profile', {
         method: 'PUT',
         data: params
     })
